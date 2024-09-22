ffc24dbe8161910988a31005e1862fc87af5a3ca
planktron22 <145958299+planktron22@users.noreply.github.com>
2024-09-22 15:54:20 +0300

    Update and rename task1 to task1.py

---
+++ week1/task1.py
@@ -0,0 +1,15 @@
+def length_of_longest_substring(s: str) -> int:
+    start = 0  
+    ml = 0  
+    used_chars = {}   
+    for i, ch in enumerate(s):
+        if ch in used_chars and start <= used_chars[ch]:
+            start = used_chars[ch] + 1       
+        used_chars[ch] = i       
+        ml = max(ml, i - start + 1)  
+    return ml
+
+s = str(input())
+print(length_of_longest_substring(s))  
+    
+        
