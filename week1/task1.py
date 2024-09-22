13762d772835fbc8dd6b4483b4c82c957ed6c31e
planktron22 <145958299+planktron22@users.noreply.github.com>
2024-09-22 16:05:12 +0300

    Update task1.py

--- week1/task1.py
+++ week1/task1.py
@@ -1,15 +1,18 @@
-def length_of_longest_substring(s: str) -> int:
-    start = 0  
-    ml = 0  
-    used_chars = {}   
-    for i, ch in enumerate(s):
-        if ch in used_chars and start <= used_chars[ch]:
-            start = used_chars[ch] + 1       
-        used_chars[ch] = i       
-        ml = max(ml, i - start + 1)  
-    return ml
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        start = 0  
+        ml = 0  
+        used_chars = {}   
+        for i, ch in enumerate(s):
+            if ch in used_chars and start <= used_chars[ch]:
+                start = used_chars[ch] + 1       
+            used_chars[ch] = i       
+            ml = max(ml, i - start + 1)  
+        return ml
 
-s = str(input())
-print(length_of_longest_substring(s))  
+sol = Solution()
+
+s = input()
+print(sol.lengthOfLongestSubstring(s))
     
         
